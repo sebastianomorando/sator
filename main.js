99b12af955a536ec578953dafaf35b640a953071
Sebastiano Morando <sebastianomorando@gmail.com>
2023-09-15 12:49:26 +0200

    no italian magic square possible with 5 length words?

--- main.js
+++ main.js
@@ -8,12 +8,22 @@ const words = require('./words.json');
     3. la seconda lettera della terza parola deve essere uguale alla terza lettera della seconda parola
 */
 
+// find all words that are the reverse of that word
+const findAllWordsThatAreTheReverseOf = (word, words) => {
+    return words.filter(w => w === word.split('').reverse().join(''));
+}
+
 const findAllWordsThatStartWith = (letter, words) => {
     return words.filter(word => word[0] === letter);
 }
 
-console.log(words[0]);
-console.log(findAllWordsThatStartWith(words[0][1], words)[0]);
+// console.log(words[0]);
+/* console.log(findAllWordsThatStartWith(words[0][1], words)[0]);
 console.log(findAllWordsThatStartWith(words[0][2], words)[0]);
 console.log(findAllWordsThatStartWith(words[0][3], words)[0]);
-console.log(findAllWordsThatStartWith(words[0][4], words)[0]);
+console.log(findAllWordsThatStartWith(words[0][4], words)[0]); */
+
+words.forEach(word => {
+    const reverse = findAllWordsThatAreTheReverseOf(word, words);
+    if (reverse.length) console.log(word, reverse);
+});
